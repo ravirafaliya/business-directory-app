37b6d507fe646a8bb5906734f1a42026b513a8be
Ravi Rafaliya <ravirr6997@gmail.com>
2025-04-17 19:27:16 +0530

    cleark setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,8 @@
+import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/clerk-expo";
 
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { Text } from "react-native";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -12,8 +14,16 @@ export default function RootLayout() {
   if (!fontsLoaded) return null;
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+
+      <SignedOut>
+        <Text>Logout</Text>
+      </SignedOut>
+    </ClerkProvider>
   );
 }
